dd87e497ff3d5875d4dfcb7dfbbaf85674920c04
Garrett Graham <10542092+ggraham@users.noreply.github.com>
2025-09-22 10:09:28 -0400

    wip nim ntpthash

---
+++ include/nimpthash.hpp
@@ -0,0 +1,95 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <tuple>
+#include "pthash.hpp"
+#include "nthash.hpp"
+namespace NtPtHash
+{
+	class GHash
+	{
+		std::unordered_set<uint64_t> m;
+		pthash::build_configuration config;
+		pthash::phobic<pthash::xxhash_128> pth;
+		uint64_t k;
+		uint64_t target_size;
+		bool built = false;
+		void ins(nthash::NtHash *h)
+		{
+			while (h->roll())
+			{
+				m.insert(h->hashes()[0]);
+			}
+		}
+		uint64_t idx(uint64_t h)
+		{
+			return pth(h);
+		}
+		std::vector<uint64_t> idx(nthash::NtHash *h)
+		{
+			std::vector<uint64_t> output;
+			while (h->roll())
+			{
+				output.push_back(idx(h->hashes()[0]));
+			}
+			return output;
+		}
+		public:
+		GHash(uint64_t k)
+			: k(k) {};
+		GHash(uint64_t k, pthash::build_configuration config)
+			: config(config), k(k) {};
+		GHash(const uint64_t k, const uint64_t num_threads, const uint64_t seed = 7, const uint64_t avg_partition_size = 100000, const double alpha = 0.97, const double lambda = 5, const bool verbose = true, const bool dense_partitioning = true)
+			: k(k)
+		{
+			config.verbose = verbose;
+			config.avg_partition_size = avg_partition_size;
+			config.alpha = alpha;
+			config.lambda = lambda;
+			config.dense_partitioning = dense_partitioning;
+			config.seed = seed;
+			config.num_threads = num_threads;
+		}
+		uint64_t getSize()
+		{
+			return target_size;
+		}
+		void ins(std::string s)
+		{
+			nthash::NtHash h(s, 1, k, 0);
+			ins(&h);
+		}
+		void ins(char *s, int l)
+		{
+			nthash::NtHash h(s, l, 1, k, 0);
+			ins(&h);
+		}
+		std::vector<uint64_t> idx(std::string s)
+		{
+			nthash::NtHash h(s, 1, k, 0);
+			if (!built)
+			{
+				build();
+			}
+			return idx(&h);
+		}
+		std::vector<uint64_t> idx(char *s, int l)
+		{
+			nthash::NtHash h(s, l, 1, k, 0);
+			if (!built)
+			{
+				build();
+			}
+			return idx(&h);
+		}
+		void build()
+		{
+			std::cout << "building map with " << m.size() << " keys" << std::endl;
+			target_size = m.size();
+			pth.build_in_internal_memory(m.begin(), target_size, config);
+			m.clear();
+			built = true;
+		}
+	};
+}
---
+++ src/nimPtHash.nim
@@ -0,0 +1,57 @@
+{.passc: "-I../include".}
+{.passl: "-L../lib -lnthash".}
+{.push header: "nimpthash.hpp".}
+type
+  CppVector*[T] {.importcpp: "std::vector<'0>", header: "<vector>".} = object
+  GHash* {.importcpp: "NtPtHash::GHash", byref.} = object
+#####
+proc newGHash(k, num_threads: uint64; seed: uint64 = 7, avg_partition_size: uint64 = 100; alpha: cdouble = 0.97, lambda: cdouble = 5; verbose: bool = true; dense_partitioning: bool = true): GHash {.importcpp: "NtPtHash::GHash(@)", constructor.}
+proc insert(h: GHash; s: pointer; l: uint64) {.importcpp: "#.ins(@)".}
+proc build(h: GHash) {.importcpp: "#.build()".}
+proc index(h: GHash; s: pointer; l: uint64): CppVector[uint64] {.importcpp: "#.idx(@)".}
+proc data[T](v: CppVector[T]): ptr T {.importcpp: "#.data()".}
+proc size(h: GHash): uint64 {.importcpp: "#.getSize()".}
+proc size[T](v: CppVector[T]): int {.importcpp: "#.size()".}
+{.pop.}
+proc index(h: GHash, s: string): seq[uint64] =
+  let
+    ac = h.index(s[0].addr, s.len.uint64)
+    length = ac.size()
+  if length == 0:
+    return @[]
+  result = newSeq[uint64](length)
+  copyMem(result[0].addr, ac.data(), length * sizeof(uint64))
+
+proc insert(h: GHash; s: string) =
+  h.insert(s[0].addr, s.len.uint64)
+
+when isMainModule:
+  import cligen, tables, strutils, strformat, klib
+  proc testStuff(f: string; k: uint64 = 12; threads: uint64 = 8) =
+    let  
+      phf = newGHash(k, threads, avg_partition_size = 10000)
+      s: string = "TGATATTATCGGTA"
+    phf.insert(s)
+    var
+      fz = xopen[GzFile](f)
+      rec: FastxRecord
+    while fz.readFastx(rec):
+      phf.insert(rec.seq)
+    phf.build()
+    
+    for h in phf.index(s):
+      stdout.writeLine "hash: {h}".fmt
+    close(fz)
+    
+    var
+      fo = xopen[GzFile](f)
+      reco: FastxRecord
+      v: seq[uint64] = newSeq[uint64](phf.size())
+    while fo.readFastx(reco):
+      for i in phf.index(reco.seq):
+        v[i]+=1
+    for n in v[0..<50]:
+      stdout.writeLine "this count: {n}".fmt
+
+
+  dispatch testStuff
